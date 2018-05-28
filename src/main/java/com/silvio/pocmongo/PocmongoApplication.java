65d000adc99f25606a7e5fa44c22f4c00912b654
SilvioCristiano <sivuca1@gmail.com>
2018-05-28 12:04:44 -0300

    primeiro commit

---
+++ src/main/java/com/silvio/pocmongo/PocmongoApplication.java
@@ -0,0 +1,12 @@
+package com.silvio.pocmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocmongoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/silvio/pocmongo/PocmongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.silvio.pocmongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocmongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
