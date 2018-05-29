7905a67f8689e6d060aef31615665e9ff461607f
SilvioCristiano <sivuca1@gmail.com>
2018-05-29 15:30:24 -0300

    PUT

---
+++ src/main/java/com/silvio/pocmongo/config/Instantiation.java
@@ -0,0 +1,60 @@
+package com.silvio.pocmongo.config;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.silvio.pocmongo.domain.Post;
+import com.silvio.pocmongo.domain.User;
+import com.silvio.pocmongo.dto.AuthorDTO;
+import com.silvio.pocmongo.dto.CommentDTO;
+import com.silvio.pocmongo.repository.PostRepository;
+import com.silvio.pocmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userReposiroty;
+
+	@Autowired
+	private PostRepository postReposiroty;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User silvio = new User(null, "Silvio Cristiano", "sivuca1@gmail.com");
+		
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob, silvio));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		CommentDTO c4 = new CommentDTO("Tenha um ótimo noite!", sdf.parse("28/02/2018"), new AuthorDTO(silvio));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3, c4));
+		
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userReposiroty.save(maria);
+	}
+
+}
